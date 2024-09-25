fa588ee56c35f9d351e3fa6148ff5f74c0b27c02
Beomky <nobleman1214@naver.com>
2024-09-25 15:48:58 +0900

    CREATE: 초기세팅

---
+++ src/main/java/kkk/dainyong/usr/login/repository/IUserRepository.java
@@ -0,0 +1,9 @@
+package kkk.dainyong.usr.login.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface IUserRepository {
+
+}
---
+++ src/test/java/kkk/dainyong/usr/UsrApplicationTests.java
@@ -0,0 +1,13 @@
+package kkk.dainyong.usr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
